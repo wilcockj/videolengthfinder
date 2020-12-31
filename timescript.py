71b638e5bab9b929c72d44d789ca2f22f52e0291
2chainzbear <jameswilcock8@gmail.com>
2020-12-30 23:08:06 -0800

    formatting file

--- timescript.py
+++ timescript.py
@@ -1,8 +1,8 @@
-from moviepy.editor import VideoFileClip
 from pathlib import Path
 import os
 from pymediainfo import MediaInfo
 import pandas as pd
+
 from tkinter import filedialog
 from tkinter import *
 root = Tk()
@@ -12,6 +12,7 @@ fixedfoldername = folder_selected.replace(':','').replace('/','-')
 print(fixedfoldername)
 
 movielist = []
+
 for r,d,f in os.walk(folder_selected):
     #make list ala gitignore for directory to ignore
     if '\\venv' not in r: 
@@ -23,10 +24,13 @@ for r,d,f in os.walk(folder_selected):
                         #make list of video titles to ignore
                         if track.duration and 'sample' not in file.lower():
                             movielist.append([file,str(track.other_duration[3][:-1])])
+
 df = pd.DataFrame(movielist,columns=['Movie Name','Duration'])
 pd.set_option('display.max_rows', df.shape[0]+1)
 df = df.sort_values('Duration')
+print(df.head())
 
 with open(f'{fixedfoldername}movielength.csv', 'w') as f:
     f.write(df.to_csv(index=False))
+
 print(f"Wrote to {fixedfoldername}movielength.csv name and length of {len(df)} videos")
