604eeafabd962cc86a202b14c9a80ca99d68a655
2chainzbear <jameswilcock8@gmail.com>
2020-12-31 00:23:45 -0800

    added average video legnth stat

--- timescript.py
+++ timescript.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from pymediainfo import MediaInfo
 import pandas as pd
+import chime
 
 from tkinter import filedialog
 from tkinter import *
@@ -13,6 +14,18 @@ print(fixedfoldername)
 
 movielist = []
 
+def convert(seconds): 
+    seconds = seconds % (24 * 3600) 
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+      
+    return "%d:%02d:%02d" % (hour, minutes, seconds) 
+      
+# Driver program 
+
+
 for r,d,f in os.walk(folder_selected):
     #make list ala gitignore for directory to ignore
     if '\\venv' not in r: 
@@ -23,14 +36,14 @@ for r,d,f in os.walk(folder_selected):
                         #print(file,track.duration)
                         #make list of video titles to ignore
                         if track.duration and 'sample' not in file.lower():
-                            movielist.append([file,str(track.other_duration[3][:-1])])
-
-df = pd.DataFrame(movielist,columns=['Movie Name','Duration'])
+                            movielist.append([file,str(track.other_duration[3][:-1]),int(float(track.duration))])
+                            break
+df = pd.DataFrame(movielist,columns=['Movie Name','Duration','MilliSecondsLong'])
 pd.set_option('display.max_rows', df.shape[0]+1)
 df = df.sort_values('Duration')
 print(df.head())
-
+print(f"Average Video length is {convert(df['MilliSecondsLong'].mean()/1000)}")
 with open(f'{fixedfoldername}movielength.csv', 'w') as f:
     f.write(df.to_csv(index=False))
-
 print(f"Wrote to {fixedfoldername}movielength.csv name and length of {len(df)} videos")
+chime.success(sync=True,raise_error=True)
